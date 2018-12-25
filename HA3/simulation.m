4b129f70b856c948de64958a16f0ef990f429b93
Yuxuan <xiayuxuan1993@outlook.com>
2018-12-25 11:44:31 +0100

    Update

--- HA3/simulation.m
+++ HA3/simulation.m
@@ -5,6 +5,7 @@ clc;clear
 RFS = multiobjectProcess();
 
 %Poisson RFS
+%2D 
 inputArg1{1} = [-1 1;-1 1];
 
 RFS = spatialDistribution(RFS,inputArg1);
@@ -12,6 +13,7 @@ lambda = 10;
 PoissonInstance = PoissonRFSs(RFS,lambda);
 
 %Bernoulli RFS
+%2D
 inputArg1{1} = [-1 1;-1 1];
 
 RFS = spatialDistribution(RFS,inputArg1);
@@ -19,6 +21,7 @@ r = 0.5;
 BernoulliInstance = BernoulliRFSs(RFS,r);
 
 %multi-Bernoulli RFS
+%2D
 inputArg1{1} = [-1 1;-1 1];
 inputArg1{2} = [-1 1;-1 1];
 
@@ -26,8 +29,10 @@ RFS = spatialDistribution(RFS,inputArg1);
 M = 2;
 r = ones(M,1)*0.5;
 multiBernoulliInstance = multiBernoulliRFSs(RFS,M,r);
+multiBernoulliInstance = cell2mat(multiBernoulliInstance');
 
 %multi-Bernoulli mixture RFS
+%2D
 inputArg1{1} = [-1 1;-1 1];
 inputArg1{2} = [-1 1;-1 1];
 inputArg1{3} = [-1 1;-1 1];
@@ -39,6 +44,7 @@ r = cell(length(M),1);
 r{1} = ones(M(1),1)*0.5;
 r{2} = ones(M(2),1)*0.5;
 multiBernoulliMixtureInstance = multiBernoulliMixtureRFSs(RFS,M,r,p);
+multiBernoulliMixtureInstance = cell2mat(multiBernoulliMixtureInstance');
 
 %% Gaussian distribution
 
@@ -47,6 +53,7 @@ clc;clear
 RFS = multiobjectProcess();
 
 %Poisson RFS
+%2D
 inputArg1{1} = [0;0];
 inputArg2{1} = eye(2);
 
@@ -55,6 +62,7 @@ lambda = 10;
 PoissonInstance = PoissonRFSs(RFS,lambda);
 
 %Bernoulli RFS
+%2D
 inputArg1{1} = [0;0];
 inputArg2{1} = eye(2);
 
@@ -63,6 +71,7 @@ r = 0.5;
 BernoulliInstance = BernoulliRFSs(RFS,r);
 
 %multi-Bernoulli RFS
+%2D
 inputArg1{1} = [0;0];
 inputArg2{1} = eye(2);
 inputArg1{2} = [0;0];
@@ -72,8 +81,10 @@ RFS = spatialDistribution(RFS,inputArg1,inputArg2);
 M = 2;
 r = ones(M,1)*0.5;
 multiBernoulliInstance = multiBernoulliRFSs(RFS,M,r);
+multiBernoulliInstance = cell2mat(multiBernoulliInstance');
 
 %multi-Bernoulli mixture RFS
+%2D
 inputArg1{1} = [0;0];
 inputArg2{1} = eye(2);
 inputArg1{2} = [0;0];
@@ -88,6 +99,7 @@ r = cell(length(M),1);
 r{1} = ones(M(1),1)*0.5;
 r{2} = ones(M(2),1)*0.5;
 multiBernoulliMixtureInstance = multiBernoulliMixtureRFSs(RFS,M,r,p);
+multiBernoulliMixtureInstance = cell2mat(multiBernoulliMixtureInstance');
 
 %% Gaussian mixture distribution
 
@@ -98,6 +110,7 @@ clc;clear
 RFS = multiobjectProcess();
 
 %Poisson RFS
+%2D
 inputArg1{1} = [0 0;1 1];
 inputArg2{1} = eye(2);      
 inputArg3{1} = [0.5 0.5];
@@ -107,6 +120,7 @@ lambda = 10;
 PoissonInstance = PoissonRFSs(RFS,lambda);
 
 %Bernoulli RFS
+%2D
 inputArg1{1} = [0 0;1 1];
 inputArg2{1} = eye(2);      
 inputArg3{1} = [0.5 0.5];
@@ -116,6 +130,7 @@ r = 0.5;
 BernoulliInstance = BernoulliRFSs(RFS,r);
 
 %multi-Bernoulli RFS
+%2D
 inputArg1{1} = [0 0;1 1];
 inputArg2{1} = eye(2);      
 inputArg3{1} = [0.5 0.5];
@@ -127,8 +142,10 @@ RFS = spatialDistribution(RFS,inputArg1,inputArg2,inputArg3);
 M = 2;
 r = ones(M,1)*0.5;
 multiBernoulliInstance = multiBernoulliRFSs(RFS,M,r);
+multiBernoulliInstance = cell2mat(multiBernoulliInstance');
 
 %multi-Bernoulli mixture RFS
+%2D
 inputArg1{1} = [0 0;1 1];
 inputArg2{1} = eye(2);      
 inputArg3{1} = [0.5 0.5];
@@ -146,3 +163,4 @@ r = cell(length(M),1);
 r{1} = ones(M(1),1)*0.5;
 r{2} = ones(M(2),1)*0.5;
 multiBernoulliMixtureInstance = multiBernoulliMixtureRFSs(RFS,M,r,p);
+multiBernoulliMixtureInstance = cell2mat(multiBernoulliMixtureInstance');
