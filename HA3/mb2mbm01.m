88c748d18d35e92e0ee11f4e30c615f5ea64d88d
yuxuanx <xiayuxuan1993@outlook.com>
2019-01-03 10:41:28 +0800

    Update

--- HA3/mb2mbm01.m
+++ HA3/mb2mbm01.m
@@ -1,6 +1,6 @@
 function [w,mbm_r,mbm_pdf] = mb2mbm01(mb_r,mb_pdf)
 %MB2MBM01 converts a multi-Bernoulli process to a multi-Bernoulli mixture
-%process with probability of existence r \in (0,1]. 
+%process with probability of existence r \in (0,1). 
 %INPUT: mb_r: probability of existence --- vector of size (number of
 %               Bernoulli components x 1).
 %       mb_pdf: object density function if exists --- struct of size (number
@@ -15,35 +15,31 @@ function [w,mbm_r,mbm_pdf] = mb2mbm01(mb_r,mb_pdf)
 %               of multi-Bernoulli components x 1). Each cell contains a 
 %               struct of size (number of Bernoulli components x 1).
 
-%NOTE: 1. If a Bernoulli process has probability of existence r = 0, it is
-%then not represented in the multi-Bernoulli process. 2. w sums up to one.
+%NOTE: 
+% 1. If a Bernoulli process has probability of existence r = 0, it is then 
+% not represented in the multi-Bernoulli process. 
+% 2. For the multi-Bernoulli included in the multi-Bernoulli mixture that has 
+% no valid probability of existence, i.e., r = 0. The returned result should 
+% satisfy that mbm_r{i} is a 0x1 empty double column vector, and that mbm_pdf{i}
+% is a 0x1 empty struct array with the same fields as the input Bernoullis.
+% 3. w sums up to one.
 
-%Put Bernoulli components with probability of existence r = 1 aside
-idx1 = mb_r == 1;
-mb_r1 = mb_r(idx1);
-mb_pdf_r1 = mb_pdf(idx1);
-
-%Select out Bernoulli components with probability of existence r < 1
-idx2 = mb_r < 1;
-mb_r = mb_r(idx2);
-mb_pdf = mb_pdf(idx2);
+num_b = length(mb_r);
 
 w = [];
 mbm_r = [];
 mbm_pdf = [];
 
-num_b = length(mb_r);
-for i = 1:num_b
+for i = 1:num_b+1
     %Note that Bernoulli components in a multi-Bernoulli are unordered
-    C = nchoosek(1:num_b,i);
+    C = nchoosek(1:num_b,i-1);
     num = size(C,1);
     mbm_r_temp = cell(num,1);
     mbm_pdf_temp = cell(num,1);
     w_temp = zeros(num,1);
     for j = 1:num
-        %Append Bernoulli components with probability of existence r = 1 
-        mbm_r_temp{j} = [mb_r1;mb_r(C(j,:))];
-        mbm_pdf_temp{j} = [mb_pdf_r1;mb_pdf(C(j,:))];
+        mbm_r_temp{j} = mb_r(C(j,:));
+        mbm_pdf_temp{j} = mb_pdf(C(j,:));
         %Calculate weights of multi-Bernoulli components
         w_temp(j) = prod(1-mb_r)*prod(mb_r(C(j,:)))/prod(1-mb_r(C(j,:)));
     end
@@ -57,4 +53,3 @@ end
 w = w/sum(w);
 
 end
-
